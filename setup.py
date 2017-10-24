608249546376f63af90f45addffc23dc2adc9978
Fabian Affolter <fabian@affolter-engineering.ch>
2017-10-24 23:07:58 +0200

    Add long description

--- setup.py
+++ setup.py
@@ -1,12 +1,18 @@
 #!/usr/bin/env python3
-
 import os
 import sys
+from codecs import open
 
 try:
     from setuptools import setup
 except ImportError:
     from distutils.core import setup
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+# Get the long description from the relevant file
+with open(os.path.join(here, 'README.rst'), encoding='utf-8') as desc:
+    long_description = desc.read()
 
 if sys.argv[-1] == 'publish':
     os.system('python3 setup.py sdist upload')
@@ -16,6 +22,7 @@ setup(
     name='netdata',
     version='0.0.1',
     description='Python API for interacting with Netdata.',
+    long_description=long_description,
     url='https://github.com/fabaff/python-netdata',
     download_url='https://github.com/fabaff/python-netdata/releases',
     author='Fabian Affolter',
