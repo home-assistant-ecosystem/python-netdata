884ed8b9adee1a2c3708a99b607408a3634344a0
Fabian Affolter <mail@fabian-affolter.ch>
2019-11-30 21:08:17 +0530

    Use black for formatting

--- netdata/__init__.py
+++ netdata/__init__.py
@@ -9,11 +9,12 @@ import async_timeout
 from . import exceptions
 
 _LOGGER = logging.getLogger(__name__)
-_INSTANCE = 'http://{host}:{port}/api/v{api}/'
-_DATA_ENDPOINT = 'data?chart={resource}&before=0&after=-1&options=seconds'
-_ALARMS_ENDPOINT = 'alarms?all&format=json'
-_ALL_METRIC_ENDPOINT = ('allmetrics?format=json&help=no&types=no&'
-                        'timestamps=yes&names=yes&data=average')
+_INSTANCE = "http://{host}:{port}/api/v{api}/"
+_DATA_ENDPOINT = "data?chart={resource}&before=0&after=-1&options=seconds"
+_ALARMS_ENDPOINT = "alarms?all&format=json"
+_ALL_METRIC_ENDPOINT = (
+    "allmetrics?format=json&help=no&types=no&" "timestamps=yes&names=yes&data=average"
+)
 
 API_VERSION = 1
 
@@ -31,26 +32,23 @@ class Netdata(object):
         self.base_url = _INSTANCE.format(host=host, port=port, api=API_VERSION)
         if data is None:
             self.endpoint = _ALL_METRIC_ENDPOINT
-        if data == 'alarms':
+        if data == "alarms":
             self.endpoint = _ALARMS_ENDPOINT
-        if data == 'data':
+        if data == "data":
             self.endpoint = _DATA_ENDPOINT
 
     async def get_data(self, resource):
         """Get detail for a resource from the data endpoint."""
-        url = '{}{}'.format(
-            self.base_url, self.endpoint.format(resource=resource))
+        url = "{}{}".format(self.base_url, self.endpoint.format(resource=resource))
 
         try:
             with async_timeout.timeout(5, loop=self._loop):
                 response = await self._session.get(url)
 
-            _LOGGER.info(
-                "Response from Netdata: %s", response.status)
+            _LOGGER.info("Response from Netdata: %s", response.status)
             data = await response.json()
             _LOGGER.debug(data)
-            self.values = {k: v for k, v in zip(
-                data['labels'], data['data'][0])}
+            self.values = {k: v for k, v in zip(data["labels"], data["data"][0])}
 
         except (asyncio.TimeoutError, aiohttp.ClientError, socket.gaierror):
             _LOGGER.error("Can not load data from Netdata")
@@ -58,14 +56,13 @@ class Netdata(object):
 
     async def get_alarms(self):
         """Get alarms for a Netdata instance."""
-        url = '{}{}'.format(self.base_url, self.endpoint)
+        url = "{}{}".format(self.base_url, self.endpoint)
 
         try:
             with async_timeout.timeout(5, loop=self._loop):
                 response = await self._session.get(url)
 
-            _LOGGER.debug(
-                "Response from Netdata: %s", response.status)
+            _LOGGER.debug("Response from Netdata: %s", response.status)
             data = await response.text()
             _LOGGER.debug(data)
             self.alarms = data
@@ -76,14 +73,13 @@ class Netdata(object):
 
     async def get_allmetrics(self):
         """Get all available metrics from a Netdata instance."""
-        url = '{}{}'.format(self.base_url, self.endpoint)
+        url = "{}{}".format(self.base_url, self.endpoint)
 
         try:
             with async_timeout.timeout(5, loop=self._loop):
                 response = await self._session.get(url)
 
-            _LOGGER.debug(
-                "Response from Netdata: %s", response.status)
+            _LOGGER.debug("Response from Netdata: %s", response.status)
             data = await response.json()
             _LOGGER.debug(data)
             self.metrics = data
