5db00753f270ab0386275773904bd551da675357
Fabian Affolter <mail@fabian-affolter.ch>
2019-11-30 21:07:27 +0530

    Use black for formatting

--- example.py
+++ example.py
@@ -9,29 +9,32 @@ from netdata import Netdata
 async def main():
     """Get the data from a Netdata instance."""
     with aiohttp.ClientSession() as session:
-        data = Netdata('localhost', loop, session, data='data')
-        await data.get_data('system.cpu')
+        data = Netdata("localhost", loop, session, data="data")
+        await data.get_data("system.cpu")
 
         print(json.dumps(data.values, indent=4, sort_keys=True))
 
         # Print the current value of the system's CPU
-        print("CPU System:", round(data.values['system'], 2))
+        print("CPU System:", round(data.values["system"], 2))
 
     with aiohttp.ClientSession() as session:
-        data = Netdata('localhost', loop, session, data='alarms')
+        data = Netdata("localhost", loop, session, data="alarms")
         await data.get_alarms()
 
         print(data.alarms)
 
     with aiohttp.ClientSession() as session:
-        data = Netdata('localhost', loop, session)
+        data = Netdata("localhost", loop, session)
         await data.get_allmetrics()
 
         print(data.metrics)
 
         # Print the current value for the system's CPU
-        print("CPU System:", round(data.metrics['system.cpu']
-                                   ['dimensions']['system']['value'], 2))
+        print(
+            "CPU System:",
+            round(data.metrics["system.cpu"]["dimensions"]["system"]["value"], 2),
+        )
+
 
 loop = asyncio.get_event_loop()
 loop.run_until_complete(main())
