d4c86cbf807b92f9f994f54b0c1cae626efba745
Fabian Affolter <fabian@affolter-engineering.ch>
2021-03-17 10:59:01 +0100

    Set aiohttp to >=3.7.4,<4
    To avoid CVE-2021-21330

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
     author='Fabian Affolter',
     author_email='fabian@affolter-engineering.ch',
     license='MIT',
-    install_requires=['aiohttp<4', 'async_timeout<4'],
+    install_requires=['aiohttp>=3.7.4,<4', 'async_timeout<4'],
     packages=['netdata'],
     zip_safe=True,
     classifiers=[
