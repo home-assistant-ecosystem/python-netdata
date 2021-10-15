a5075e2fe1d20de1b689b18d528da4bbded515ef
Fabian Affolter <mail@fabian-affolter.ch>
2021-10-15 02:46:46 +0200

    Update example

--- example.py
+++ example.py
@@ -1,6 +1,5 @@
 """Sample code to interact with a Netdata instance."""
 import asyncio
-import aiohttp
 import json
 
 from netdata import Netdata
@@ -8,18 +7,27 @@ from netdata import Netdata
 
 async def main():
     """Get the data from a Netdata instance."""
-    async with aiohttp.ClientSession() as session:
-        data = Netdata("localhost", loop, session)
-        # Get data for the CPU
-        await data.get_data("system.cpu")
-        print(json.dumps(data.values, indent=4, sort_keys=True))
+    client = Netdata("localhost")
 
-        # Print the current value of the system's CPU
-        print("CPU System:", round(data.values["system"], 2))
+     # Get all metrics
+    await client.get_info()
+    print(client.info)
 
-        # Get the alarms which are present
-        await data.get_alarms()
-        print(data.alarms)
+    # Get details of a available chart
+    await client.get_chart("system.cpu")
+    print(json.dumps(client.values, indent=4, sort_keys=True))
+
+    # Print the current value of the system's CPU
+    print("CPU System:", round(client.values["system"], 2))
+
+    # Get the alarms which are present
+    await client.get_alarms()
+    print(client.alarms)
+
+    # Get all metrics
+    await client.get_allmetrics()
+    print(client.metrics)
+
 
 loop = asyncio.get_event_loop()
 loop.run_until_complete(main())
